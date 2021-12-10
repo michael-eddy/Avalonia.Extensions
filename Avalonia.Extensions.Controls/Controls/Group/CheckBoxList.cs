eca914d67944452f81bbac1d64d2982c30dc7239
dinghaoxiang <michael19950415@outlook.com>
2021-12-10 18:00:23 +0800

    group

---
+++ Avalonia.Extensions.Controls/Controls/Group/CheckBoxList.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Extensions.Styles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalonia.Extensions.Controls
+{
+    public class CheckBoxList : ItemsRepeater, IStyling
+    {
+
+    }
+}
---
+++ Avalonia.Extensions.Controls/Controls/Group/RadioButtonList.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Extensions.Styles;
+using System;
+
+namespace Avalonia.Extensions.Controls
+{
+    public class RadioButtonList : ItemsRepeater, IStyling
+    {
+        private string GroupId { get; }
+        public RadioButtonList()
+        {
+            GroupId = Guid.NewGuid().ToString("N");
+            ItemsProperty.Changed.AddClassHandler<RadioButtonList>(OnItemsChanged);
+        }
+        private void OnItemsChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
