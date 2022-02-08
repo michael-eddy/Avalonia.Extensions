694f637a65a374fb1a11a3621ca8fdaba40f88de
dinghaoxiang <michael19950415@outlook.com>
2022-02-08 17:15:24 +0800

    Text

---
+++ Avalonia.Extensions.Controls/Controls/Text/Text.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Avalonia.Extensions.Controls
+{
+    public sealed class Text : Control
+    {
+        public static readonly StyledProperty<string> ContentProperty =
+            AvaloniaProperty.Register<Text, string>("Content");
+        public string Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+        public Text()
+        {
+            AffectsRender<Text>(ContentProperty);
+        }
+        public override void Render(DrawingContext context)
+        {
+            var formattedText = new FormattedText(Content, Typeface.Default, 12, TextAlignment.Left,
+                 TextWrapping.Wrap, new Size(300, 300));
+            context.DrawText(new SolidColorBrush(Colors.Transparent), new Point(10, 0), formattedText);
+        }
+    }
+}
