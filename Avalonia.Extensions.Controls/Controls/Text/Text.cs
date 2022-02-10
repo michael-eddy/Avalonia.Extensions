0b1141dbafac25de3bb76021b315d572f2ae66eb
dinghaoxiang <michael19950415@outlook.com>
2022-02-10 16:20:21 +0800

    Update Text.cs

--- Avalonia.Extensions.Controls/Controls/Text/Text.cs
+++ Avalonia.Extensions.Controls/Controls/Text/Text.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls.Shapes;
 using Avalonia.Media;
+using Avalonia.Metadata;
 using System;
 using System.Drawing;
 
@@ -7,26 +8,48 @@ namespace Avalonia.Extensions.Controls
 {
     public sealed class Text : Shape
     {
-        public static readonly StyledProperty<string> ContentProperty = AvaloniaProperty.Register<Text, string>(nameof(Content));
+        /// <summary>
+        /// Defines the <see cref="Content"/> property
+        /// </summary>
+        public static readonly DirectProperty<Text, string?> ContentProperty =
+            AvaloniaProperty.RegisterDirect<Text, string?>(nameof(Content), o => o.Content, (o, v) => o.Content = v);
+        /// <summary>
+        /// Gets or sets the content to display in this flyout
+        /// </summary>
+        [Content]
         public string Content
         {
-            get => GetValue(ContentProperty);
-            set => SetValue(ContentProperty, value);
+            get { return _content; }
+            set { SetAndRaise(ContentProperty, ref _content, value); }
         }
+        /// <summary>
+        /// Defines the <see cref="Foreground"/> property.
+        /// </summary>
         public static readonly StyledProperty<IBrush> ForegroundProperty = AvaloniaProperty.Register<Text, IBrush>(nameof(Foreground), new SolidColorBrush(Colors.Black));
+        /// <summary>
+        /// Gets or sets a brush used to paint the text.
+        /// </summary>
         public IBrush Foreground
         {
             get => GetValue(ForegroundProperty);
             set => SetValue(ForegroundProperty, value);
         }
+        /// <summary>
+        /// Defines the <see cref="FontSize"/> property.
+        /// </summary>
         public static readonly StyledProperty<int> FontSizeProperty = AvaloniaProperty.Register<Text, int>(nameof(FontSize), 14);
+        /// <summary>
+        /// Gets or sets the font size.
+        /// </summary>
         public int FontSize
         {
             get => GetValue(FontSizeProperty);
             set => SetValue(FontSizeProperty, value);
         }
+        private string _content;
         public Text()
         {
+            _content = string.Empty;
             AffectsRender<Text>(ContentProperty);
             AffectsRender<Text>(ForegroundProperty);
             SetValue(StrokeThicknessProperty, 2);
