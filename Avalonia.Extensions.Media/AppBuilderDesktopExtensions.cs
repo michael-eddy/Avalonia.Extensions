b6a3902a051d01caa7913e0a85547ab48ed3c8df
dinghaoxiang <michael19950415@outlook.com>
2022-03-08 18:02:33 +0800

    playerView

---
+++ Avalonia.Extensions.Media/AppBuilderDesktopExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using LibVLCSharp.Shared;
+using System;
+
+namespace Avalonia.Extensions
+{
+    public static class AppBuilderDesktopExtensions
+    {
+        public static TAppBuilder UseVlcPlayer<TAppBuilder>(this TAppBuilder builder, params Type[] supportContols)
+       where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        {
+            builder.AfterSetup((_) => { Core.Initialize(); });
+            return builder;
+        }
+    }
+}
---
+++ Avalonia.Extensions.Media/Controls/PlayerView.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+
+namespace Avalonia.Extensions.Media
+{
+    public class PlayerView : TemplatedControl
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            DrawTemplate();
+        }
+        private void DrawTemplate()
+        {
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+            grid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+
+
+
+            Template = new FuncControlTemplate((_, _) => new Decorator { Child = grid });
+            ApplyTemplate();
+        }
+    }
+}
