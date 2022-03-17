39efb3d4610c19f7bb28dbcab83ff2cf177b2653
Ding <michael19950415@outlook.com>
2022-03-18 00:16:39 +0800

    add webview

---
+++ Avalonia.Extensions.WebView/AppBuilderDesktopExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging;
+using CefNet;
+using System;
+
+namespace Avalonia.Extensions.WebView
+{
+    public static class AppBuilderDesktopExtensions
+    {
+        internal static CefNetApplication CefApp { get; private set; }
+        public static TAppBuilder UseWebView<TAppBuilder>(this TAppBuilder builder, string cefPath, CefSettings cefSettings)
+           where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        {
+            builder.AfterSetup((_) =>
+            {
+                try
+                {
+                    CefApp = new CefNetApplication();
+                    CefApp.Initialize(cefPath, cefSettings);
+                    if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                        desktop.Exit += Desktop_Exit;
+                }
+                catch (Exception ex)
+                {
+                    Logger.TryGet(LogEventLevel.Error, LogArea.Control)?.Log(builder, ex.Message);
+                    throw;
+                }
+            });
+            return builder;
+        }
+        private static void Desktop_Exit(object sender, ControlledApplicationLifetimeExitEventArgs e)
+        {
+            try
+            {
+                CefApp.Shutdown();
+                CefApp.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Logger.TryGet(LogEventLevel.Error, LogArea.Control)?.Log(sender, ex.Message);
+            }
+        }
+    }
+}
---
+++ Avalonia.Extensions.WebView/WebViewProcess.cs
@@ -0,0 +1,23 @@
+﻿using CefNet;
+
+namespace Avalonia.Extensions.WebView
+{
+    public sealed class WebViewProcess
+    {
+        private static WebViewProcess instance;
+        public static WebViewProcess Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new WebViewProcess();
+                return instance;
+            }
+        }
+        private WebViewProcess()
+        {
+
+        }
+        public static CefNetApplication CefApp => AppBuilderDesktopExtensions.CefApp;
+    }
+}
