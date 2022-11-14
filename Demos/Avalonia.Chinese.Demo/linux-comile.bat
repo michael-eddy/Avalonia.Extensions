405c42fc03cca7c5cca2f629958c47eadb992fe9
Ding <michael19950415@outlook.com>
2022-11-14 17:52:18 +0800

    v1.3.0

--- Demos/Avalonia.Chinese.Demo/linux-comile.bat
+++ Demos/Avalonia.Chinese.Demo/linux-comile.bat
@@ -7,6 +7,7 @@ set Buildx64=%root%\linux-x64
 echo ÕýÔÚ±àÒëx64°æ±¾...
 dotnet restore -r linux-x64
 dotnet deb install
-dotnet msbuild Avalonia.Extensions.Demo.csproj -property:OutDir=%Buildx64% -t:CreateDeb -p:TargetFramework=net6.0 -p:RuntimeIdentifier=linux-x64 -p:Configuration=Debug
+dotnet msbuild Avalonia.Chinese.Demo.csproj -property:OutDir=%Buildx64% -t:CreateDeb -p:TargetFramework=net6.0 -p:RuntimeIdentifier=linux-x64 -p:Configuration=Debug
 move %Buildx64%\*.deb %root%\
 rd /s/q %Buildx64%
+pause
